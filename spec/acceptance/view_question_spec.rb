814ff3da49d2c6797e7cb5d05846711d16fdd152
Basov_ae <bainst4@gmail.com>
2015-11-17 10:52:07 +0500

    added test for view quwstion

---
+++ spec/acceptance/view_question_spec.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+require 'rails_helper'
+
+feature 'View question', '
+  In order to solve problem
+  As a guest
+  I want to be able to view existing questions
+' do
+
+  scenario 'guest views question' do
+    question = create(:question)
+    answer = create(:answer, question: question)
+
+    visit questions_path
+    click_on question.title
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+    expect(page).to have_content answer.body
+  end
+end
