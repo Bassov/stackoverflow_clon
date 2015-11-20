98a0da398076a312467b9eb544170ba48f5cbf97
Basov_ae <bainst4@gmail.com>
2015-11-20 13:43:26 +0500

    refactoring

--- spec/acceptance/question/list_questions_spec.rb
+++ spec/acceptance/question/list_questions_spec.rb
@@ -6,9 +6,10 @@ feature 'list questions', '
   As an guest
   I can view questions page
 ' do
-  scenario 'guest visit questions page' do
-    questions = create_list(:question, 2)
+  given(:user) { create(:user) }
+  given!(:questions) { create_list(:question, 2, user: user) }
 
+  scenario 'guest visit questions page' do
     visit questions_path
 
     questions.each { |question| expect(page).to have_content question.title }
