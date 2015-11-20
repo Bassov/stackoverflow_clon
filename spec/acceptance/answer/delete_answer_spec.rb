4146e73b5a5434f09f73d7d6ebbacfd6942baead
Basov_ae <bainst4@gmail.com>
2015-11-20 11:11:25 +0500

    fixed to not show delete button to non-author

--- spec/acceptance/answer/delete_answer_spec.rb
+++ spec/acceptance/answer/delete_answer_spec.rb
@@ -8,10 +8,10 @@ feature 'Delete answer', '
 ' do
   given(:user) { create(:user) }
   given(:another_user) { create(:user) }
+  given!(:question) { create(:question) }
 
   scenario 'Author of answer deletes answer' do
     sign_in(user)
-    question = create(:question)
     create(:answer, question: question, user: user)
 
     visit questions_path
@@ -20,17 +20,5 @@ feature 'Delete answer', '
 
     expect(current_path).to eq question_path(question)
     expect(page).to have_content 'Ответ успешно удален'
-  end
-
-  scenario 'Non-author of answer tries to delete answer' do
-    sign_in(user)
-    question = create(:question)
-    create(:answer, question: question, user: another_user)
-
-    visit questions_path
-    click_on question.title
-    page.first(:link, 'Удалить').click
-
-    expect(page).to have_content 'Вы не являетесь автором ответа'
   end
 end
