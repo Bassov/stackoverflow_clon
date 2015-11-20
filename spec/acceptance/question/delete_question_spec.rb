a13252332e4f4ee94e91748f45a60405e99fec3d
Basov_ae <bainst4@gmail.com>
2015-11-20 13:16:55 +0500

    refactoring

--- spec/acceptance/question/delete_question_spec.rb
+++ spec/acceptance/question/delete_question_spec.rb
@@ -8,10 +8,10 @@ feature 'Delete question', '
 ' do
   given(:user) { create(:user) }
   given(:another_user) { create(:user) }
+  given!(:question) { create(:question, user: user) }
 
   scenario 'Author of question deletes question' do
     sign_in(user)
-    question = create(:question, user: user)
 
     visit questions_path
     click_on question.title
@@ -19,16 +19,6 @@ feature 'Delete question', '
 
     expect(current_path).to eq questions_path
     expect(page).to have_content 'Успешно удалено'
-  end
-
-  scenario 'Non-author of question tries to delete question' do
-    sign_in(user)
-    question = create(:question, user: another_user)
-
-    visit questions_path
-    click_on question.title
-    click_on 'Удалить'
-
-    expect(page).to have_content 'Вы не являетесь автором вопроса'
+    expect(page).to_not have_content question.title
   end
 end
