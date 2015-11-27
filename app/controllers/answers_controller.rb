05dedc2abbdc51508f0f28c14ba98789ca6a7fc9
Basov_ae <bainst4@gmail.com>
2015-11-27 15:28:23 +0500

    removed before_actions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,6 @@
 # encoding: utf-8
 class AnswersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_answer, only: :destroy
-  before_action :check_authority, only: :destroy
 
   def create
     @question = Question.find(params[:question_id])
@@ -12,19 +10,13 @@ class AnswersController < ApplicationController
   end
 
   def destroy
+    @answer = Answer.find(params[:id])
+    return redirect_to :back, notice: 'Вы не являетесь автором вопроса' unless current_user.author_of(@answer)
     @answer.destroy
     redirect_to :back, notice: 'Ответ успешно удален'
   end
 
   private
-
-  def set_answer
-    @answer = Answer.find(params[:id])
-  end
-
-  def check_authority
-    redirect_to :back, notice: 'Вы не являетесь автором вопроса' unless current_user.author_of(@answer)
-  end
 
   def answer_params
     params.require(:answer).permit(:body)
