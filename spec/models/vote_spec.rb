af87b4019a631c60bce5f6c9e59614caf41d28ec
Basov_ae <bainst4@gmail.com>
2015-12-21 08:59:48 +0500

    added test for validation

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
   it { should validate_presence_of :user_id }
+  it { should validate_uniqueness_of(:user_id).scoped_to([:votable_type, :votable_id]) }
 
   it { should belong_to :votable}
   it { should belong_to :user }
