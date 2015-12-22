a896a503a5e66ef77c440c417e4a2ffd2cbe8dcd
Basov_ae <bainst4@gmail.com>
2015-12-22 14:37:20 +0500

    author of votable cant vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,7 @@
 class VotesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_votable
+  before_action :check_authority
 
   def vote_up
     current_user.vote_for(@votable, 1)
@@ -19,6 +20,10 @@ class VotesController < ApplicationController
   end
 
   private
+
+  def check_authority
+    render :vote if current_user.author_of?(@votable)
+  end
 
   def set_votable
     klass = params[:votable_type].to_s.capitalize.constantize
