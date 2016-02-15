135f0c419c22cccd4752262b6aba19a64944fbdf
Basov_ae <bainst4@gmail.com>
2016-02-15 19:52:32 +0500

    randomized tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,7 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.order = "random"
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryGirl::Syntax::Methods
