d0a95c510237418c4104bb47d32fbceb558f3257
Basov_ae <bainst4@gmail.com>
2016-02-21 18:03:01 +0500

    added index to API (lists profiles of all users)

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -1,39 +1,57 @@
 require 'rails_helper'
 
 describe 'Profile API' do
+  context 'unauthorized' do
+    it 'returns 401 status if there no access_token' do
+      get '/api/v1/profiles/me', format: :json
+      expect(response.status).to eq 401
+    end
+
+    it 'returns 401 status if access_token is invalid ' do
+      get '/api/v1/profiles/me', format: :json, access_token: '1234'
+      expect(response.status).to eq 401
+    end
+  end
+
+  let(:me) { create :user }
+  let(:access_token) { create :access_token, resource_owner_id: me.id }
+
   describe 'GET /me' do
-     context 'unauthorized' do
-       it 'returns 401 status if there no access_token' do
-         get '/api/v1/profiles/me', format: :json
-         expect(response.status).to eq 401
-       end
+    before { get '/api/v1/profiles/me', format: :json, access_token: access_token.token }
 
-       it 'returns 401 status if access_token is invalid ' do
-         get '/api/v1/profiles/me', format: :json, access_token: '1234'
-         expect(response.status).to eq 401
-       end
-     end
+    it 'returns status 200' do
+      expect(response).to be_success
+    end
 
-    context 'authorized' do
-      let(:me) { create :user }
-      let(:access_token) { create :access_token, resource_owner_id: me.id }
-
-      before { get '/api/v1/profiles/me', format: :json, access_token: access_token.token }
-
-      it 'returns status 200' do
-        expect(response).to be_success
+    %w(id email created_at updated_at admin).each do |attr|
+      it "contains #{attr}" do
+        expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr)
       end
+    end
 
-      %w(id email created_at updated_at admin).each do |attr|
-        it "contains #{attr}" do
-          expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr)
-        end
+    %w(password encrypted_password).each do |attr|
+      it "does not contain #{attr}" do
+        expect(response.body).to_not have_json_path(attr)
       end
+    end
+  end
 
-      %w(password encrypted_password).each do |attr|
-        it "does not contain #{attr}" do
-          expect(response.body).to_not have_json_path(attr)
-        end
+  describe 'GET /all' do
+    let!(:users) { create_list(:user, 3) }
+
+    before { get '/api/v1/profiles', format: :json, access_token: access_token.token }
+
+    it 'returns status 200' do
+      expect(response.status).to eq 200
+    end
+
+    it 'returns list of users' do
+      expect(response.body).to be_json_eql(users.to_json).at_path('users')
+    end
+
+    %w(password encrypted_password).each do |attr|
+      it "does not contain #{attr}" do
+        expect(response.body).to_not have_json_path(attr)
       end
     end
   end
