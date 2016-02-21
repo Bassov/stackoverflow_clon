f795703b9a2fe0675a588f65f58d331b891a52e1
Basov_ae <bainst4@gmail.com>
2016-02-21 17:19:52 +0500

    added profiles api

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,13 @@ Rails.application.routes.draw do
     patch :create_vote, on: :member
   end
 
+  namespace :api do
+    namespace :v1 do
+      resource :profiles do
+        get :me, on: :collection
+      end
+    end
+  end
+
   root 'questions#index'
 end
