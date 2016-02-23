8b4a68e3d8561aad6e4f90217a8705ec2d4cec8e
Basov_ae <bainst4@gmail.com>
2016-02-23 10:10:42 +0500

    added answer/index API

---
+++ app/controllers/api/v1/answers_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::AnswersController < Api::V1::BaseController
+  before_action :set_question
+
+  def index
+    respond_with @question.answers, each_serializer: AnswersListSerializer
+  end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+end
