89ff74275714c6caeb22552face77edc401af06d
Basov_ae <bainst4@gmail.com>
2016-02-29 22:01:32 +0500

    added shared attachable test and added dependent destroy dependencies

--- app/models/concerns/attachable.rb
+++ app/models/concerns/attachable.rb
@@ -2,7 +2,7 @@
 module Attachable
   extend ActiveSupport::Concern
   included do
-    has_many :attachments, as: :attachable
+    has_many :attachments, as: :attachable, dependent: :destroy
     accepts_nested_attributes_for :attachments, allow_destroy: true, reject_if: :all_blank
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,7 +4,7 @@ class Question < ActiveRecord::Base
 
   has_many :answers, dependent: :destroy
   has_many :votes, as: :votable
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
   belongs_to :user
 
   validates :title, :body, :user_id, presence: true
