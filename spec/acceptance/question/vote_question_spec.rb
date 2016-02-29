a57d7328b3e8b6a34447fe907c8750f9eca3acb1
Basov_ae <bainst4@gmail.com>
2016-02-29 21:13:16 +0500

    added shared tests for adding votes

--- spec/acceptance/question/vote_question_spec.rb
+++ spec/acceptance/question/vote_question_spec.rb
@@ -1,22 +1,21 @@
 # encoding: utf-8
 require_relative '../acceptance_helper'
 
-feature 'Vote answer', '
-  In order to mark answers
+feature 'Vote question', '
+  In order to mark question
   As an authenticated user
-  I want to be able to create for answers
+  I want to be able to vote for question
 ' do
   given(:user) { create(:user) }
-  given!(:question) { create(:question) }
-  given!(:user_answer) { create(:answer, user: user, question: question) }
-  given!(:answer) { create(:answer, question: question) }
-  given(:selector) { "#answer_#{answer.id}" }
+  given(:question) { create(:question) }
+  given(:user_question) { create :question, user: user }
+  given(:selector) { '.question' }
 
   it_behaves_like 'Acceptance votable'
 
   scenario 'Author of votable cant vote for it' do
     sign_in user
-    visit question_path(question)
+    visit question_path(user_question)
 
     within selector do
       expect(page).to_not have_content '+'
