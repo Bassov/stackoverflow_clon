88c835941045f2f1c14bc2b47a3e4d0cd801e6ff
Basov_ae <bainst4@gmail.com>
2016-03-07 13:28:33 +0500

    added subscription model and association

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe Question, type: :model do
   it { should have_many :votes }
   it { should have_many(:comments).dependent(:destroy) }
 
+  it { should have_many(:subscriptions).dependent(:destroy) }
+  it { should have_many(:subscribers).through(:subscriptions).source(:user) }
+
   it { should belong_to :user }
 
   it { should validate_presence_of :title }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe User, type: :model do
   it { should have_many :comments }
   it { should have_many :authorizations }
 
+  it { should have_many(:subscriptions).dependent(:destroy) }
+  it { should have_many(:subscribes).through(:subscriptions).source(:question) }
+
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
